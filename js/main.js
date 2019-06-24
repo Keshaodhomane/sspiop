58adfadd596c99a3c7ef3076eee848c98a57c156
Keshao Dhomane <keshaodhomane@gmail.com>
2019-06-24 21:36:47 +0530

    main

--- js/main.js
+++ js/main.js
@@ -148,6 +148,44 @@ $(document).ready(function() {
 
 
 
+      var t = new TimelineMax({paused:true});
+
+      t.set(".img1",{display:"none"})
+      t.set(".img2",{display:"none"})
+      t.set(".img3",{display:"none"})
+      t.set(".img4",{display:"none"})
+      t.set(".imgsbanner div h2",{display:"none"})
+      t.set(".imgsbanner div h3",{display:"none"})
+
+      t.from(".img1", 1, {display:"block", autoAlpha:0, scale:5, ease:Sine.easeOut})
+      t.from(".img1 h2", .5, {display:"block", autoAlpha:0, left:"-300px", ease:Sine.easeOut})
+      t.from(".img1 h3", .5, {display:"block", autoAlpha:0, left:"-300px", ease:Sine.easeOut})
+      t.to("img1",1,{autoAlpha:0},"+=2")
+
+
+      t.from(".img2", 1, {display:"block", autoAlpha:0, scale:5, ease:Sine.easeOut})
+      t.from(".img2 h2", .5, {display:"block", autoAlpha:0, left:"-300px", ease:Sine.easeOut})
+      t.from(".img2 h3", .5, {display:"block", autoAlpha:0, left:"-300px", ease:Sine.easeOut})
+      t.to("img2",1,{autoAlpha:0},"+=2")
+
+
+      t.from(".img3", 1, {display:"block", autoAlpha:0, scale:5, ease:Sine.easeOut})
+      t.from(".img3 h2", .5, {display:"block", autoAlpha:0, left:"-300px", ease:Sine.easeOut})
+      t.from(".img3 h3", .5, {display:"block", autoAlpha:0, left:"-300px", ease:Sine.easeOut})
+      t.to("img3",1,{autoAlpha:0},"+=2")
+      
+
+      t.from(".img4", 1, {display:"block", autoAlpha:0, scale:5, ease:Sine.easeOut})
+      t.from(".img4 h2", .5, {display:"block", autoAlpha:0, left:"-300px", ease:Sine.easeOut})
+      t.from(".img4 h3", .5, {display:"block", autoAlpha:0, left:"-300px", ease:Sine.easeOut})
+      t.to("img4",1,{autoAlpha:0},"+=2")
+
+
+      t.play();
+      t.repeat(-1);
+
+
+
 
    
 });
