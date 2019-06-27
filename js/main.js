9ff346d906cba81f0f27db6f7683f97e9b161bb2
Keshao Dhomane <keshaodhomane@gmail.com>
2019-06-27 21:27:33 +0530

    MAIN

--- js/main.js
+++ js/main.js
@@ -17,45 +17,39 @@ $(document).ready(function() {
 
 
 
-
-    $('.slider-for').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        fade: true,
-        asNavFor: '.slider-nav'
-      });
-
-      $('.slider-nav').slick({
+    $('.autoplay').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
-        asNavFor: '.slider-for',
-        centerMode: true,
-        focusOnSelect: true,
-
+        autoplay: true,
+        autoplaySpeed: 2000,
         responsive: [
             {
-              breakpoint: 768,
+              breakpoint: 1024,
               settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 2
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
               }
             },
             {
               breakpoint: 480,
               settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 1
+                slidesToShow: 1,
+                slidesToScroll: 1
               }
             }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
           ]
-
       });
 
 
